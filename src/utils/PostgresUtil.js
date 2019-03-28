57f7e95d493c35e88a68aba456478aa85eeed6e5
kh3996 <kh3996@gmail.com>
2019-03-27 22:48:10 -0300

    postgresql setting

--- src/utils/PostgresUtil.js
+++ src/utils/PostgresUtil.js
@@ -4,11 +4,11 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   // ssl: true,
   user: 'osys1000',
-  host: 'localhost',
+  host: '127.0.0.1',
   database: 'chat_server',
   password: '1q1q1q1q',
   port: '5432'
-})
+});
 
 module.exports = {
   pool,

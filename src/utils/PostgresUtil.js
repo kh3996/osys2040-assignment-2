1f91b52ff82bde7247d968a2255215099cf55f5a
kh3996 <kh3996@gmail.com>
2019-03-29 22:11:12 -0300

    test

--- src/utils/PostgresUtil.js
+++ src/utils/PostgresUtil.js
@@ -1,14 +1,15 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+//  connectionString: process.env.DATABASE_URL,
 //  ssl: true,
-//  user: 'osys1000',
+  user: 'osys1000',
+//	user: 'postgres',
 //  host: '127.0.0.1',
-//  host: 'localhost',
-//  database: 'chat_server',
-//  password: '1q1q1q1q',
-//  port: '5432'
+  host: 'localhost',
+  database: 'chatserver',
+  password: '1q1q1q1q',
+  port: '5432',
 })
 
 module.exports = {
